07b162f51aaedcf4b1b8011322af5f6c88224f2f
Abhishek <abhishek.magdum.07@gmail.com>
2023-04-14 19:15:17 +0530

    Added : Addition

---
+++ src/test/java/com/practice/DemoTest.java
@@ -0,0 +1,15 @@
+package com.practice;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DemoTest {
+    Demo runner = new Demo();
+
+    @Test
+    void addition(){
+        assertEquals(5,runner.add(3,5));
+    }
+
+}
